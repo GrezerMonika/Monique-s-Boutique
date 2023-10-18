341f5b048bb9e5c16838b15bfc2ff7f06135632e
GrezerMonika <131771568+GrezerMonika@users.noreply.github.com>
2023-10-18 18:37:53 +0300

    video-03-create-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Monique's Boutique</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
